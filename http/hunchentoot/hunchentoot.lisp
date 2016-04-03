4c8df34eee4f9ccde1a56bd999fd22b581059241
Ilya Khaprov <dead.trickster@gmail.com>
2016-04-03 15:45:47 -0700

    can mute access/messages logs

--- http/hunchentoot/hunchentoot.lisp
+++ http/hunchentoot/hunchentoot.lisp
@@ -1,7 +1,19 @@
 (in-package #:prometheus.hunchentoot)
 
 (defclass hunchentoot-exposer ()
-  ((metrics-path :initform #P"metrics" :initarg :metrics-path :reader metrics-path)))
+  ((metrics-path :initform #P"metrics" :initarg :metrics-path :reader metrics-path)
+   (mute-access-logs :initform t :initarg :mute-access-logs :reader mute-access-logs)
+   (mute-messages-logs :initform t :initarg :mute-error-logs :reader mute-messages-logs)))
+
+(defmethod tbnl:acceptor-log-access ((acceptor hunchentoot-exposer) &key return-code)
+  (declare (ignore return-code))
+  (unless (mute-access-logs acceptor)
+    (call-next-method)))
+
+(defmethod tbnl:acceptor-log-message ((acceptor hunchentoot-exposer) log-level format-string &rest format-arguments)
+  (declare (ignore log-level format-string format-arguments))
+  (unless (mute-messages-logs acceptor)
+    (call-next-method)))
 
 (defmethod tbnl:acceptor-dispatch-request ((acceptor hunchentoot-exposer) request)
   (if (equal (metrics-path acceptor) (tbnl:request-pathname request))
