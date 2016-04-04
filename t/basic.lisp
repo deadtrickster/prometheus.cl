c419bdc6c3e3f94e7ddafe16cc1bbe57b85e08fa
Ilya Khaprov <dead.trickster@gmail.com>
2016-04-04 02:05:30 -0700

    Add summary to basic test

--- t/basic.lisp
+++ t/basic.lisp
@@ -6,18 +6,20 @@
   `(let ((prom:*default-registry* (make-instance 'prom:registry)))
      ,@body))
 
-(subtest "Basic counter/gauge test"
+(subtest "Basic test"
   (with-fresh-registry
     (let ((rc (prom:make-counter :name "requests_counter" :help "Hunchentoot requires counter" :labels '("type")))
           (tmg (prom:make-gauge :name "total_memory" :help "SBCL total memory"))
-          (h (prom:make-histogram :name "render_time" :help "" :labels '("type") :buckets '(2 4 6))))
-      (prom:counter.inc rc :n 5 :labels '("get"))
-      (prom:counter.inc rc :n 12 :labels '("post"))
+          (h (prom:make-histogram :name "render_time" :help "" :labels '("type") :buckets '(2 4 6)))
+          (s (prom:make-summary :name "traffic_summary" :help "traffic summary" :value 12)))
+      (prom:counter.inc rc :value 5 :labels '("get"))
+      (prom:counter.inc rc :value 12 :labels '("post"))
       (prom:gauge.set tmg 566)
       (prom:histogram.observe h 4.5 :labels '("html"))
       (prom:histogram.observe h 1 :labels '("html"))
       (prom:histogram.observe h 0.5 :labels '("html"))
       (prom:histogram.observe h 4.5 :labels '("pdf"))
+      (prom:summary.observe s 43.3d0)
       (is (prometheus.text:marshal) "# TYPE requests_counter counter
 # HELP requests_counter Hunchentoot requires counter
 requests_counter{type=\"post\"} 12
@@ -31,14 +33,18 @@ render_time_bucket{type=\"pdf\", le=\"2\"} 0
 render_time_bucket{type=\"pdf\", le=\"4\"} 0
 render_time_bucket{type=\"pdf\", le=\"6\"} 1
 render_time_bucket{type=\"pdf\", le=\"+Inf\"} 1
-render_time_sum{type=\"pdf\"} 4.5d0
+render_time_sum{type=\"pdf\"} 4.5
 render_time_count{type=\"pdf\"} 1
 render_time_bucket{type=\"html\", le=\"2\"} 2
 render_time_bucket{type=\"html\", le=\"4\"} 2
 render_time_bucket{type=\"html\", le=\"6\"} 3
 render_time_bucket{type=\"html\", le=\"+Inf\"} 3
-render_time_sum{type=\"html\"} 6.0d0
+render_time_sum{type=\"html\"} 6.0
 render_time_count{type=\"html\"} 3
+# TYPE traffic_summary summary
+# HELP traffic_summary traffic summary
+traffic_summary_sum 55.3
+traffic_summary_count 2
 "))))
 
 (finalize)
