37d6d6448b751626d52aebc47a9d8b9c0c6cf6dc
Ilya Khaprov <dead.trickster@gmail.com>
2016-04-06 13:55:46 -0700

    allow count in make-summary

--- src/metrics/summary.lisp
+++ src/metrics/summary.lisp
@@ -13,24 +13,27 @@
   ((value :initform +summary-default+)
    (sum :initform +summary-default+ :reader summary-sum)))
 
-(defgeneric summary.observe (summary value &key labels)
-  (:method ((summary summary) value &key labels)
+(defgeneric summary.observe% (summary value count labels)
+  (:method ((summary summary) value count labels)
     (synchronize summary
       (let ((metric (get-metric summary labels)))
-        (summary.observe metric value))))
-  (:method ((summary summary-metric) value &key labels)
+        (summary.observe% metric value count nil))))
+  (:method ((summary summary-metric) value count labels)
     (declare (ignore labels))
     (synchronize summary
-      (incf (slot-value summary 'value))
+      (incf (slot-value summary 'value) count)
       (incf (slot-value summary 'sum) value))))
 
-(defun make-summary (&key name help labels value (registry *default-registry*))
+(defun summary.observe (summary value &key (count 1) labels)
+  (summary.observe% summary value count labels))
+
+(defun make-summary (&key name help labels value (count 1) (registry *default-registry*))
   (check-value-or-labels value labels)
   (let ((summary (make-instance 'summary :name name
                                          :help help
                                          :labels labels)))
     (when value
-      (summary.observe summary value))
+      (summary.observe summary value :count count))
     (when registry
       (register summary registry))
     summary))
