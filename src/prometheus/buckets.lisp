3fcf73f6952007e836dbf71d7f248425e4263e91
Ilya Khaprov <dead.trickster@gmail.com>
2016-06-12 14:35:06 +0300

    fix typo

--- src/prometheus/buckets.lisp
+++ src/prometheus/buckets.lisp
@@ -1,6 +1,6 @@
 (in-package #:prometheus)
 
-(defun try-to-maintain-intger-bounds (bound)
+(defun try-to-maintain-integer-bounds (bound)
   (let ((boundf (floor bound)))
     (if (= bound boundf)
         boundf
@@ -10,7 +10,7 @@
   (assert (> count 1) (count) 'invalid-value-error :value count :reason "buckets count should be positive")
 
   (loop repeat count
-        collect (try-to-maintain-intger-bounds (prog1 start
+        collect (try-to-maintain-integer-bounds (prog1 start
                                                  (incf start step)))))
 
 (defun generate-exponential-buckets (start factor count)
@@ -19,5 +19,5 @@
   (assert (> factor 1) (factor) 'invalid-value-error :value factor :reason "buckets factor should be greater than 1")
 
   (loop repeat count
-        collect (try-to-maintain-intger-bounds (prog1 start
+        collect (try-to-maintain-integer-bounds (prog1 start
                                                  (setf start (* start factor))))))
