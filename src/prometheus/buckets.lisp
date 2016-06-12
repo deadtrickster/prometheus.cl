f984e072934e7be908213ffc9eaec02af1c6e03d
Ilya Khaprov <dead.trickster@gmail.com>
2016-06-12 14:34:50 +0300

    exponential bucket factor should be greater than 1

--- src/prometheus/buckets.lisp
+++ src/prometheus/buckets.lisp
@@ -16,7 +16,7 @@
 (defun generate-exponential-buckets (start factor count)
   (assert (> count 1) (count) 'invalid-value-error :value count :reason "buckets count should be positive")
   (assert (> start 0) (start) 'invalid-value-error :value start :reason "buckets start should be positive")
-  (assert (>= factor 1) (factor) 'invalid-value-error :value factor :reason "buckets factor should be greater than 1")
+  (assert (> factor 1) (factor) 'invalid-value-error :value factor :reason "buckets factor should be greater than 1")
 
   (loop repeat count
         collect (try-to-maintain-intger-bounds (prog1 start
